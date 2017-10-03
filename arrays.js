9ce929193608058cb6d782ad923bb5aa1510a625
Lauren Perchitti <laurenperchitti@gmail.com>
2017-10-03 18:28:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,6 @@
 
 
-var chocolateBars = [
-    "snickers"
-    "hundred grand"
-    "kitkat"
-    'skittles'
-]
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", 'skittles']
 
 
 function addElementToBeginningOfArray(array, element) {
